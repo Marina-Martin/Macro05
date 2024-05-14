29b6f773a37bf45b053e939fad657cdba6318ac6
Mariaki <mari.m4rtin@gmail.com>
2024-05-14 15:26:12 -0300

    adicionar tela principal com fundo e title

--- Treat/Treat/ViewController.swift
+++ Treat/Treat/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let titleLabel: UILabel = {
+        let title = UILabel()
+        
+        title.text = "Suas Atividades"
+        title.textColor = UIColor(named: "AppWhite")
+        title.font = UIFont(name: "RadioCanada-Regular_bold", size: 32)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        
+        return title
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor(named: "AppBlack")
+        
+        setConstraints()
     }
 
-
+    func setConstraints(){
+        setTitleLabel()
+    }
+    
+    func setTitleLabel() {
+        view.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20)
+        ])
+    }
 }
 
+#Preview(){
+    return ViewController()
+}
